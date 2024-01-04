799dd801d8d9d428af1e3d00ad65306516a913bd
Natalie_S <statasha2013@gmail.com>
2024-01-04 18:38:12 +0200

    Change function "useEffect"

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ function App() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(json => setData(json))
-            .catch(error => console.error('Error:', error));
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const json = await response.json();
+                setData(json);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
     }, []);
 
     return (
